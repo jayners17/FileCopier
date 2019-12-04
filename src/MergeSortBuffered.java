a31875b9f35cb57324b6fe0803bdbac4e3698825
Ricky He <rickyhe@my.bridgeport.edu>
2019-12-04 12:04:45 -0500

    Mergesort

--- src/MergeSortBuffered.java
+++ src/MergeSortBuffered.java
@@ -64,16 +64,6 @@ public class MergeSortBuffered {
     }
 
     public static void sortBlock(RandomFileBuffer2 buff) throws IOException {
-        /*int[] arr = new int[AMT_OF_INTEGERS/4];
-        int mid = arr.length/2;
-        while (!buff.full()) {
-            buff.append(mergeFile.readInt());
-        }
-        buff.flush();
-        buff.fill();
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = buff.read();
-        }*/
         /**
          * Fills the buffer and sorts it
          */
@@ -89,7 +79,7 @@ public class MergeSortBuffered {
         buff.flush();
 
     }
-    public static void mergeSort(byte[] a){
+/*    public static void mergeSort(byte[] a){
         if (a.length < 2){
             return;
         }
@@ -127,6 +117,70 @@ public class MergeSortBuffered {
         }
         while(j < right){
             a[k++] = rightArray[j++];
+        }
+    }*/
+    public static void mergeSortReal()throws IOException{
+        for (int pass = 0, count = 1; pass < maxPass;  pass++, count *=2){
+            split(F,A,B,count);
+            merge(F,A,B,count);
+        }
+    }
+    private void split(RandomAccessFile F,RandomAccessFile A, RandomAccessFile B, int n)throws IOException{
+        F.seek(0);
+        A.seek(0);
+        B.seek(0);
+
+        for (int i = 0; i < size / (2 * n); i++) {
+            for (int j = 0; j < n; j++) {
+                A.writeInt(F.readInt());
+            }
+            for (int k = 0; k < n; k++) {
+                B.writeInt(F.readInt());
+            }
+        }
+    }
+    private void merge(RandomAccessFile F, RandomAccessFile A, RandomAccessFile B, int n)throws  IOException{
+        int a,b;
+        int aCount = 0, bCount = 0;
+
+        F.seek(0);
+        A.seek(0);
+        B.seek(0);
+
+        for (int i = 0; i < size / (2 * n); i++) {
+            aCount = bCount = n;
+            a = A.readInt();
+            b = B.readInt();
+
+            while (aCount != 0 && bCount != 0){
+                if ( a < b){
+                    F.writeInt(a);
+                    aCount--;
+
+                    if (aCount != 0){
+                        a = A.readInt();
+                    }
+                }else {
+                    F.writeInt(b);
+                    bCount--;
+
+                    if (bCount != 0){
+                        b = B.readInt();
+                    }
+                }
+            }
+            while(aCount-- != 0){
+                F.writeInt(a);
+                if (aCount != 0){
+                    a = A.readInt();
+                }
+            }
+            while (bCount-- != 0) {
+                F.writeInt(b);
+                if (bCount != 0) {
+                    b = B.readInt();
+                }
+            }
         }
     }
 
